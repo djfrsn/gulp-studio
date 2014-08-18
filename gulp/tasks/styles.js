b9f732dff1656015b38beff8f1fd664aec4a1d20
Dennis Jefferson <dj@dennisjefferson.com>
2014-08-17 21:47:31 -0700

    Stability update - browsersync related

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,7 +2,7 @@
  // Process Styles w/ the exclusion of critical css
 var gulp = require('gulp');
 
-gulp.task('compile-sass', function () {
+gulp.task('styles', function () {
   var filterCritical = gulpFilter(['*', '!app/styles/critical/**/*.scss']);
 
     return gulp.src(sourced.sass)
@@ -10,19 +10,8 @@ gulp.task('compile-sass', function () {
         .pipe(filterCritical)
         .pipe(plumber({errorHandler: notify.onError()}))
         .pipe(sass({sourcemap: true, sourcemapPath: '.', style: 'compact'}))
-        .pipe(gulp.dest(sourced.styles));
+        .pipe(gulp.dest(sourced.styles))
+        .pipe(browserSync.reload({stream:true, once: true}));
 });
 
 
-
-gulp.task('sync-css', function () {
-
-    return gulp.src(sourced.css)
-           .pipe(browserSync.reload({stream:true, once: true}))
-           .pipe(gulp.dest(sourced.styles));
-});
-
-gulp.task('styles', function(callback) {
-        runSequence( 'compile-sass', 'sync-css',
-                  callback);
-});
