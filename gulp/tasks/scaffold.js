de172bc23b6e7b05696e0d7d531f6ec77f961eaf
Dennis Jefferson <dj@dennisjefferson.com>
2014-08-17 21:29:20 -0700

    Update stability with browser-sync adjustment

--- gulp/tasks/scaffold.js
+++ gulp/tasks/scaffold.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'),
 	remember = require('gulp-remember'), 
 	rename = require("gulp-rename");
 
-gulp.task('compile-scaffold', function() {
+gulp.task('scaffold', function() {
 
 	return gulp.src(sourced.html)
 		.pipe(changed(sourced.includes))
@@ -11,18 +11,6 @@ gulp.task('compile-scaffold', function() {
 		.pipe(plumber({errorHandler: notify.onError()}))
 		.pipe(fileinclude())
 		.pipe(rename("index.html"))
-		.pipe(browserSync.reload({stream:true, once: true}))
-		.pipe(gulp.dest(sourced.app));
-});
-
-gulp.task('sync-css', function () {
-
-    return gulp.src(sourced.css)
-           .pipe(reload({stream:true}))
-           .pipe(gulp.dest(sourced.styles));
-});
-
-gulp.task('scaffold', function(callback) {
-        runSequence( 'compile-scaffold', 'sync-css',
-                  callback);
+		.pipe(gulp.dest(sourced.app))
+		.pipe(browserSync.reload({stream:true, once: true}));
 });
