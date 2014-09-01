395740ff324cb73da3ebe62d847d4fe8eb058266
Dennis Jefferson <dj@dennisjefferson.com>
2014-09-01 13:32:58 -0700

    Patch scaffolding browser reload glitch

--- gulp/tasks/paint.js
+++ gulp/tasks/paint.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp');
 
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('paint-scaffold', function(callback) {
+		runSequence( ['scaffold', 'bs-reload'],
+	      [ 'scaffold', 'bs-reload' ],
+	      callback);
+});
+
 gulp.task('paint', [ 'setWatch', 'browser-sync'], function () {
     gulp.watch([ sourced.sass, sourced.ignoreCritCSS, sourced.ignoreCritSASS],
      	['styles']);
     gulp.watch([ sourced.html ],
-    	['scaffold']);
+    	['paint-scaffold']);
     gulp.watch([ sourced.jsdir],
     	['brush']);
 });
